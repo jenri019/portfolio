690d649cc266eddefd68474c34ce959c4cf6a173
jenri019 <j.enrique209436@hotmail.com>
2025-04-02 16:49:37 -0600

    feat: adding routes, showing info in to main page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./pages/main/main.component').then(m => m.MainComponent)
+    },
+    {
+        path: '**', redirectTo: '', pathMatch: 'full'
+    },
+];
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { AboutComponent } from "../../components/about/about.component";
+import { ProjectsComponent } from "../../components/projects/projects.component";
+import { SkillsComponent } from "../../components/skills/skills.component";
+import { ContactComponent } from "../../components/contact/contact.component";
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [AboutComponent, ProjectsComponent, SkillsComponent, ContactComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
