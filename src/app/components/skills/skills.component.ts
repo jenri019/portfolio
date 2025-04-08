02eac7d0da530edf70462832886380bb57504026
jenri019 <j.enrique209436@hotmail.com>
2025-04-08 17:20:34 -0600

    feat: modifying how info is showed

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
@@ -5,11 +6,31 @@ import { TranslateModule } from '@ngx-translate/core';
   selector: 'app-skills',
   standalone: true,
   imports: [
+    CommonModule,
     TranslateModule
   ],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-
+  skills: string[] = [
+    'Angular',
+    'TypeScript',
+    'JavaScript',
+    'HTML',
+    'CSS',
+    'Sass',
+    'Bootstrap',
+    'Tailwind CSS',
+    'Node.js',
+    'Express.js',
+    'MongoDB',
+    'MySQL',
+    'PostgreSQL',
+    'Git',
+    'GitHub',
+    'RESTful APIs',
+    'GraphQL'
+  ];
+  duplicatedSkills = [...this.skills, ...this.skills]; // Duplica los elementos para el efecto
 }
