a0b0fdd44cc83acae9f9dcb6c59995d04284dff9
jenri019 <j.enrique209436@hotmail.com>
2025-04-11 14:28:12 -0600

    feat: adding tech icons, showing it

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -3,34 +3,35 @@ import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-skills',
-  standalone: true,
-  imports: [
-    CommonModule,
-    TranslateModule
-  ],
-  templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+    selector: 'app-skills',
+    standalone: true,
+    imports: [
+        CommonModule,
+        TranslateModule
+    ],
+    templateUrl: './skills.component.html',
+    styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-  skills: string[] = [
-    'Angular',
-    'TypeScript',
-    'JavaScript',
-    'HTML',
-    'CSS',
-    'Sass',
-    'Bootstrap',
-    'Tailwind CSS',
-    'Node.js',
-    'Express.js',
-    'MongoDB',
-    'MySQL',
-    'PostgreSQL',
-    'Git',
-    'GitHub',
-    'RESTful APIs',
-    'GraphQL'
-  ];
-  duplicatedSkills = [...this.skills, ...this.skills]; // Duplica los elementos para el efecto
+    technologies: { icon: string; name: string }[] = [
+        { icon: 'angular', name: 'Angular' },
+        { icon: 'bootstrap', name: 'Bootstrap' },
+        { icon: 'capacitor', name: 'Capacitor' },
+        { icon: 'django', name: 'Django' },
+        { icon: 'figma', name: 'Figma' },
+        { icon: 'git', name: 'Git' },
+        { icon: 'javascript', name: 'JavaScript' },
+        { icon: 'mui', name: 'Material UI' },
+        { icon: 'mysql', name: 'MySQL' },
+        { icon: 'ngrx', name: 'NgRx' },
+        { icon: 'nodedotjs', name: 'Node.js' },
+        { icon: 'php', name: 'PHP' },
+        { icon: 'python', name: 'Python' },
+        { icon: 'react', name: 'React' },
+        { icon: 'redux', name: 'Redux' },
+        { icon: 'sequelize', name: 'Sequelize' },
+        { icon: 'sqlite', name: 'SQLite' },
+        { icon: 'tailwindcss', name: 'Tailwind CSS' },
+        { icon: 'typescript', name: 'TypeScript' }
+    ];
 }
