4e15d0bd88eb7c8b213f843c2884caeee3c3bfbc
jenri019 <j.enrique209436@hotmail.com>
2025-04-12 17:50:38 -0600

    feat: adding option to change language

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,20 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-navbar',
     imports: [
-        CommonModule
+        CommonModule,
+        MatMenuModule,
+        TranslateModule
     ],
     templateUrl: './navbar.component.html',
     styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
     navigation:string[] = [
-        'user',
-        'skills',
-        'projects',
-        'experience',
-        'contact'
+        'language',
+        'download'
     ];
 }
