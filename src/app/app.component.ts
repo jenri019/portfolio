63380e481e2ba6d08e7d5baa367e5ef15d128296
jenri019 <j.enrique209436@hotmail.com>
2025-04-15 20:19:50 -0600

    feat: adding meta tags translate, removing non usee components, modifying  styles imports, adding global classes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MetaService } from './services/meta.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    RouterOutlet
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [
+        RouterOutlet
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'portfolio';
+    title = 'portfolio';
+
+    constructor(
+        private metaService: MetaService,
+        private translate: TranslateService // Añade esto
+    ) { }
+
+    ngOnInit() {
+        this.translate.onLangChange.subscribe(() => { // Espera cambios de idioma
+            this.updateMeta();
+        });
+
+        this.updateMeta(); // Llama la primera vez
+    }
+
+    private updateMeta() {
+        this.metaService.updateMetaTags({
+            title: 'meta.title',          // Clave para el título (traducido)
+            description: 'meta.description', // Clave para la descripción (traducida)
+            keywords: 'meta.keywords' // Opcional: palabras clave (traducido)
+        });
+    }
 }
