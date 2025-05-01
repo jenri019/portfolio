5a83d2fa07662ce986a4601610fcb7b347285d31
jenri019 <j.enrique209436@hotmail.com>
2025-05-01 15:35:26 -0600

    feat: showing experieence & project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AboutComponent } from "./components/about/about.component";
 import { HeaderComponent } from "./components/header/header.component";
+import { ProjectsComponent } from "./components/projects/projects.component";
+import { ExperienceComponent } from "./components/experience/experience.component";
 
 @Component({
     selector: 'app-root',
-    imports: [AboutComponent, HeaderComponent],
+    imports: [AboutComponent, HeaderComponent, ProjectsComponent, ExperienceComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
