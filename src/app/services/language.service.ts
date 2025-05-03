a87f0e7225c06e13cccc90c4a1c62061db0ef3a7
jenri019 <j.enrique209436@hotmail.com>
2025-05-03 15:48:59 -0600

    feat: removing localstorage langage option

--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -7,32 +7,14 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class LanguageService {
     _translate = inject(TranslateService);
-    private readonly LANGUAGE_KEY = 'app_language';
-    private availableLanguages = ['es-MX', 'en-US'] as const;
 
     // Signal para el idioma actual
-    currentLanguage = signal<'es-MX' | 'en-US'>(
-        this.getLanguageFromStorage() || 'es-MX'
-    );
-
-    constructor() {
-        // Efecto para persistir cambios en localStorage
-        effect(() => {
-            localStorage.setItem(this.LANGUAGE_KEY, this.currentLanguage());
-        });
-    }
+    currentLanguage = signal<'es-MX' | 'en-US'>('es-MX');
 
     toggleLanguage() {
         this.currentLanguage.update(current =>
             current === 'es-MX' ? 'en-US' : 'es-MX'
         );
         this._translate.use(this.currentLanguage());
-    }
-
-    private getLanguageFromStorage(): 'es-MX' | 'en-US' | null {
-        const lang = localStorage.getItem(this.LANGUAGE_KEY);
-        return this.availableLanguages.includes(lang as any)
-            ? lang as 'es-MX' | 'en-US'
-            : null;
     }
 }
