45bffcf0d178d40f7f6aa7f49e52d72e66ce82d1
jenri019 <j.enrique209436@hotmail.com>
2025-05-06 17:19:22 -0600

    feat: adding my skills component

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -13,30 +13,36 @@ import { TranslateModule } from '@ngx-translate/core';
     styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-    technologies: { icon: string; name: string }[] = [
-        { icon: 'angular', name: 'Angular' },
-        { icon: 'cognito', name: 'AWS Cognito' },
-        { icon: 'bootstrap', name: 'Bootstrap' },
-        { icon: 'capacitor', name: 'Capacitor' },
-        { icon: 'css', name: 'CSS' },
-        { icon: 'django', name: 'Django' },
-        { icon: 'figma', name: 'Figma' },
-        { icon: 'git', name: 'Git' },
-        { icon: 'html', name: 'HTML' },
-        { icon: 'javascript', name: 'JavaScript' },
-        { icon: 'mui', name: 'Material UI' },
-        { icon: 'mysql', name: 'MySQL' },
-        { icon: 'ngrx', name: 'NgRx' },
-        { icon: 'node', name: 'Node.js' },
-        { icon: 'php', name: 'PHP' },
-        { icon: 'python', name: 'Python' },
-        { icon: 'react', name: 'React' },
-        { icon: 'redux', name: 'Redux' },
-        { icon: 'sequelize', name: 'Sequelize' },
-        { icon: 'sqlite', name: 'SQLite' },
-        { icon: 'sqlserver', name: 'SQL Server' },
-        { icon: 'tailwindcss', name: 'Tailwind CSS' },
-        { icon: 'typescript', name: 'TypeScript' },
-        { icon: 'zap', name: 'OWASP-ZAP' }
+    technologies: { icon: string }[] = [
+        { icon: 'Angular' },
+        { icon: 'Angular Material' },
+        { icon: 'Cognito' },
+        { icon: 'Bootstrap' },
+        { icon: 'Capacitor' },
+        { icon: 'CSS3' },
+        { icon: 'Django' },
+        { icon: 'Figma' },
+        { icon: 'Git' },
+        { icon: 'HTML5' },
+        { icon: 'JavaScript' },
+        { icon: 'Material UI' },
+        { icon: 'MySQL' },
+        { icon: 'NgRx' },
+        { icon: 'NodeJS' },
+        { icon: 'PHP' },
+        { icon: 'Python' },
+        { icon: 'React' },
+        { icon: 'Redux' },
+        { icon: 'Sequelize' },
+        { icon: 'SQLite' },
+        { icon: 'SQL Server' },
+        { icon: 'Tailwind CSS' },
+        { icon: 'TypeScript' },
+        { icon: 'OWASP ZAP' }
     ];
+/*     duplicatedTechnologies: any[] = [];
+
+    ngOnInit() {
+      this.duplicatedTechnologies = [...this.technologies, ...this.technologies];
+    } */
 }
